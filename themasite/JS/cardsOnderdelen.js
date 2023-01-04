c471ae63bbcb0b695614d6f493660c4ff522a0d0
SiebeDemetser <siebedemetser@gmail.com>
2023-01-04 12:35:51 +0100

    started testing with code snippet

--- themasite/JS/cardsOnderdelen.js
+++ themasite/JS/cardsOnderdelen.js
@@ -1,12 +1,16 @@
 const cards = document.querySelectorAll('.card');
-const details = document.query
+    cards.forEach(card => {
+        const button = card.querySelector('.card-button');
+        button.addEventListener('click', () => {
+            const title = card.querySelector('.card-title').textContent;
+            const summaries = document.querySelectorAll('summary');
+            const content = Array.from(summaries).filter(summary => summary.textContent === title)[0].parentElement;
+            content.style.display = 'block';
+            content.setAttribute('open', '');
 
-cards.forEach(card => {
-    const button = card.querySelector('.card-button');
-    button.addEventListener('click', () => {
-      const title = card.querySelector('.card-title').textContent;
-      const content = document.querySelector(`summary:contains(${title})`).parentElement;
-      content.style.display = 'block';
+            // Close all other details elements
+            document.querySelectorAll('details').forEach(el => {
+                if (el !== content) el.style.display = 'none';
+            });
+        });
     });
-  });
-  
